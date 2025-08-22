7e37db92152f57d19b7b12a4d7f6d92858fef2b9
Dom Jackson <dom.jackson@bbc.co.uk>
2025-08-22 11:48:01 +0100

    Initial Commit

---
+++ flashcards/flashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  flashcardsApp.swift
+//  flashcards
+//
+//  Created by Dom Jackson on 22/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct flashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
